afaa37c0e276d0e0de16471f001ccb304f65c70c
Paula Cano <paulacanoroman@gmail.com>
2023-12-10 19:44:03 +0100

    feat: create and linked component

---
+++ components/InputDeleteWords.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const InputDeleteWords = () => {
+  return (
+    <div>InputDeleteWords</div>
+  )
+}
+
+export default InputDeleteWords
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import styles from '@/styles/Home.module.css'
 import ChangeColor from '@/components/ChangeColor'
+import InputDeleteWords from '@/components/InputDeleteWords'
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
       </Head>
       <main className={`${styles.main}`}>
         <ChangeColor></ChangeColor>
+        <InputDeleteWords></InputDeleteWords>
       </main>
     </>
   )
