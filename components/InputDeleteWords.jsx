c89eb43798c1e88db8ead5d9951b87b644229635
Paula Cano <paulacanoroman@gmail.com>
2023-12-10 19:52:40 +0100

    feat:  structure input and button

--- components/InputDeleteWords.jsx
+++ components/InputDeleteWords.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const InputDeleteWords = () => {
   return (
-    <div>InputDeleteWords</div>
+    <div>
+        <h3> Removes repeated words </h3>
+        <input onChange={(e)=>{console.log(e.target.value)}}></input>
+        <br/>
+        <button onClick={()=>{}}>Delete</button>
+    </div>
   )
 }
 
