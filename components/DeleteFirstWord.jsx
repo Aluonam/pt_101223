a4259325b731493e90e61829dfa556a88df19be6
Paula Cano <paulacanoroman@gmail.com>
2023-12-10 21:08:30 +0100

    feat: delete first word with .shift()

--- components/DeleteFirstWord.jsx
+++ components/DeleteFirstWord.jsx
@@ -3,12 +3,20 @@ import React, { useState } from 'react'
 const DeleteFirstWord = () => {
 
     const [inputSentence, setInputSentence] = useState([])
+
+    const handleDelete = ()=>{
+        const words = inputSentence.split(" ")
+        words.shift()
+        setInputSentence( words.join(" "))
+    }
+
   return (
     <div>
         <h3>Delete first word</h3>
-        {inputSentence}
         <input onChange={(e)=>{setInputSentence(e.target.value)}}></input>
-        <button onClick={()=>{}}>Delete</button>
+        <button onClick={()=>{handleDelete()}}>Delete</button>
+        <br/>
+        {inputSentence}
     </div>
   )
 }
