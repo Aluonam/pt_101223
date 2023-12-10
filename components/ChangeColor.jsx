b4de5f7e179599109327aaeb9150a3675364cb8e
Paula Cano <paulacanoroman@gmail.com>
2023-12-10 19:18:57 +0100

    feat: input show color in console

--- components/ChangeColor.jsx
+++ components/ChangeColor.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 
 const ChangeColor = () => {
+
+
   return (
-    <div>ChangeColor</div>
+    <div>
+        <input type='color' onChange={(e)=>{console.log(e.target.value)}}></input>
+    </div>
   )
 }
 
